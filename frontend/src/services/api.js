250eeb4b073def3fa3d7374c12786f282f0095f0
Lipsa Mohanty <lipsamohanty139@gmail.com>
2026-06-17 15:53:08 +0530

    Updated complete EMS Project

---
+++ frontend/src/services/api.js
@@ -0,0 +1,5 @@
+import axios from'axios';
+const api=axios.create({baseURL:'http://localhost:8080',headers:{'Content-Type':'application/json'}});
+api.interceptors.request.use(c=>{const t=localStorage.getItem('ems_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c;});
+api.interceptors.response.use(r=>r,e=>{if(e.response?.status===401){localStorage.clear();window.location.href='/login';}return Promise.reject(e);});
+export default api;
